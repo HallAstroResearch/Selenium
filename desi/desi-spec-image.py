6c45fc3d71cd50b8bbbe3f62266544414dc1d30a
yorkphall <yorkphall@gmail.com>
2025-12-01 14:12:53 -0500

    PBH edits

--- desi/desi-spec-image.py
+++ desi/desi-spec-image.py
@@ -11,6 +11,20 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+# Program to download and optionally rename DESI spectrum images
+# 
+# DONE:
+# * Accept SDSS_NAME and use as beginning of output filenema
+# * list each image to terminal as it's created
+# 
+# TO DO:
+# * Answer "PBH ? for ZZZ" question below: had to changet netmonitor keys, but what does it do?
+# * Rework to accept input list including SDSS_NAME,targetid
+# * Output list of DESI spectrum images ( list to screen each one as it's created)
+# * Add option to not display in firefox, or only display two DESI spectrum images at a time in firefox (currently loading one & previous one)
+# * Goal is to create DESi spectrum images for all objects in AWTQ_DESI_20250703.xlsx on SharePoint
 
 from base64 import b64decode as base64decode
 from builtins import isinstance as isa
@@ -24,7 +38,7 @@ from selenium.common import TimeoutException
 from selenium.webdriver import Firefox, FirefoxOptions, Keys
 from selenium.webdriver import Remote as Browser
 
-
+# Misc. functions
 def isapple() -> bool:
 	return PLATFORM == "darwin"
 
@@ -49,11 +63,15 @@ def write(f: Path | str, x: bytes | str) -> int:
 
 # mypy: disable-error-code="func-returns-value"
 
+# PBH ? for ZZZ: what is this for? Had to simplify as shown for program to run
 if isapple():
-	netmonitor = Keys.LEFT_COMMAND + Keys.LEFT_OPTION + "E"
+	#netmonitor = Keys.LEFT_COMMAND + Keys.LEFT_OPTION + "E"
+	netmonitor = Keys.SHIFT + "E"
 else:
-	netmonitor = Keys.LEFT_CONTROL + Keys.LEFT_SHIFT + "E"
+	#netmonitor = Keys.LEFT_CONTROL + Keys.LEFT_SHIFT + "E"
+	netmonitor = Keys.SHIFT + "E"
 
+# Set up Firefox browser
 def init(headless: bool = False) -> Browser:
 	# https://wiki.mozilla.org/Firefox/CommandLineOptions
 	opt = FirefoxOptions()
@@ -84,6 +102,7 @@ def init(headless: bool = False) -> Browser:
 	ret.set_window_size(1600, 900) if headless else None
 	return ret
 
+# Load spectrum image from a given data release (dr) of a given DESI targetid (id) 
 def load(br: Browser, dr: Literal["edr", "dr1"], id: int | str) -> bytes:
 	# https://data.desi.lbl.gov/doc/access/
 	br.get(f"https://www.legacysurvey.org/viewer/desi-spectrum/{dr}/targetid{id}")
@@ -93,8 +112,11 @@ def load(br: Browser, dr: Literal["edr", "dr1"], id: int | str) -> bytes:
 	rv = replace(r"^data:[^,]*,", "", rv)
 	return base64decode(rv)
 
-def save(br: Browser, dr: Literal["edr", "dr1"], id: int | str, path: str | None = None) -> None:
-	if path is None: path = f"desi-{dr}-{id}.png"
+# Save spectrum image to disk
+def save(br: Browser, sdss_name: str, dr: Literal["edr", "dr1"], id: int | str, path: str | None = None) -> None:
+#def save(br: Browser, dr: Literal["edr", "dr1"], id: int | str, path: str | None = None) -> None:
+	#if path is None: path = f"desi-{dr}-{id}.png"
+	if path is None: path = f"{sdss_name}-desi-{dr}-{id}.png"
 	br.switch_to.new_window() # new tab
 	if filesize(path) > 0: return br.get(Path(path).absolute().as_uri()) # already exists
 	br.get("about:logo"), br.switch_to.active_element.send_keys(netmonitor), sleep(1)
@@ -104,10 +126,15 @@ def save(br: Browser, dr: Literal["edr", "dr1"], id: int | str, path: str | None
 			break
 		except TimeoutException: sleep(2)
 	write(path, data)
+	print(f"{path} created")
 
+# Main program loop
 if __name__ == "__main__":
 	ff = init()
-	save(ff, "dr1", 39627848784286649) # https://www.legacysurvey.org/viewer/desi-spectrum/dr1/targetid39627848784286649
-	save(ff, "dr1", 39627848784285507) # https://www.legacysurvey.org/viewer/desi-spectrum/dr1/targetid39627848784285507
+	save(ff, "081148.27+083240.1", "dr1", 39627991554195594) # https://www.legacysurvey.org/viewer/desi-spectrum/dr1/targetid39627848784286649
+	#save(ff, "152348.99-004701.8", "dr1", 39627770480824813) # https://www.legacysurvey.org/viewer/desi-spectrum/dr1/targetid39627848784286649
+	#save(ff, "124652.81-081312.9", "dr1", 39627589169449230) # https://www.legacysurvey.org/viewer/desi-spectrum/dr1/targetid39627848784286649
+	##save(ff, "dr1", 39627848784286649) # https://www.legacysurvey.org/viewer/desi-spectrum/dr1/targetid39627848784286649
+	##save(ff, "dr1", 39627848784285507) # https://www.legacysurvey.org/viewer/desi-spectrum/dr1/targetid39627848784285507
 	# ff.quit() # close the browser
 
